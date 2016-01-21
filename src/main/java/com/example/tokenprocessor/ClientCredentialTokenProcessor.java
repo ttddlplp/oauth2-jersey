9c102a9cb48a0aac7dbb0144c7d91cc8b1003b98
ttddlplp <ttddlplp@gmail.com>
2016-01-21 16:35:27 +0000

    more client credential token

--- src/main/java/com/example/tokenprocessor/ClientCredentialTokenProcessor.java
+++ src/main/java/com/example/tokenprocessor/ClientCredentialTokenProcessor.java
@@ -4,10 +4,13 @@ import com.example.OAuthRequestWrapper;
 import org.apache.oltu.oauth2.as.request.AbstractOAuthTokenRequest;
 import org.apache.oltu.oauth2.as.request.OAuthTokenRequest;
 import org.apache.oltu.oauth2.as.response.OAuthASResponse;
+import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
+import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Response;
+import java.util.UUID;
 
 public class ClientCredentialTokenProcessor implements TokenRequestProcessor {
     private final Verifier verifier;
@@ -24,13 +27,16 @@ public class ClientCredentialTokenProcessor implements TokenRequestProcessor {
                 OAuthResponse response = OAuthASResponse
                         .tokenResponse(HttpServletResponse.SC_OK)
                         .setExpiresIn("3600")
-                        .setAccessToken("token")
+                        .setAccessToken(UUID.randomUUID().toString())
                         .buildJSONMessage();
                 return Response.status(response.getResponseStatus()).entity(response.getBody()).build();
+            } else {
+                return Response.status(Response.Status.UNAUTHORIZED).build();
             }
-        } catch (Exception e) {
+        } catch (OAuthProblemException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Invalid request.").build();
+        } catch (OAuthSystemException e) {
             throw new RuntimeException(e);
         }
-        return null;
     }
 }
--- src/test/java/com/example/tokenprocessor/ClientCredentialTokenProcessorTest.java
+++ src/test/java/com/example/tokenprocessor/ClientCredentialTokenProcessorTest.java
@@ -42,4 +42,28 @@ public class ClientCredentialTokenProcessorTest {
         assertThat(response.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
         assertThatJson(response.getEntity()).node("access_token").isPresent();
     }
+
+    @Test
+    public void incorrectClientIdAndSecrect() throws Exception {
+        when(request.getParameter("client_id")).thenReturn(Common.CLIENT_ID);
+        when(request.getParameter("client_secret")).thenReturn(Common.CLIENT_SECRET);
+        when(request.getParameter("grant_type")).thenReturn(GrantType.CLIENT_CREDENTIALS.toString());
+        when(request.getMethod()).thenReturn("POST");
+        when(request.getContentType()).thenReturn("application/x-www-form-urlencoded");
+        when(verifier.checkClient(anyString(), anyString())).thenReturn(false);
+        Response response = processor.process(request);
+        assertThat(response.getStatus()).isEqualTo(Response.Status.UNAUTHORIZED.getStatusCode());
+        assertThatJson(response.getEntity()).node("access_token").isAbsent();
+    }
+
+    @Test
+    public void badRequest() throws Exception {
+        when(request.getParameter("client_id")).thenReturn(null);
+        when(request.getParameter("client_secret")).thenReturn(null);
+        when(request.getParameter("grant_type")).thenReturn(null);
+        when(request.getMethod()).thenReturn(null);
+        when(request.getContentType()).thenReturn("application/x-www-form-urlencoded");
+        Response response = processor.process(request);
+        assertThat(response.getStatus()).isEqualTo(Response.Status.BAD_REQUEST.getStatusCode());
+    }
 }
